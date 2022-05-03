df79d8b008629a2e554c66ad51bd8a49c9c98349
durjoybd14 <nrd141913@gmail.com>
2022-05-03 16:12:15 +0600

    meta check2

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,66 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <meta charset="utf-8" />
+        <title>Meta Check</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="apple-mobile-web-app-title" content="Meta-Check" />
+        <meta name="application-name" content="Meta-Check" />
+        <meta
+          name="description"
+          content="BasaKhujo is an accommodation solution provider platform that makes it possible to rent a house & mess without paying any brokerage."
+        />
+        <meta
+          itemProp="name"
+          content="BasaKhujo: A property searching platform in Bangladesh"
+        />
+        <meta
+          itemProp="description"
+          content="BasaKhujo is an accommodation solution provider platform that makes it possible to rent a house & mess without paying any brokerage."
+        />
+        <meta
+          itemProp="image"
+          content="https://res.cloudinary.com/nomidman/image/upload/v1651493754/icon-192x192_xjiau9.webphttps://res.cloudinary.com/nomidman/image/upload/v1651510593/icon-192x192_zcm7e9.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@meta-check" />
+        <meta
+          name="twitter:title"
+          content="BasaKhujo: A property searching platform in Bangladesh"
+        />
+        <meta
+          name="twitter:description"
+          content="BasaKhujo is an accommodation solution provider platform that makes it possible to rent a house & mess without paying any brokerage."
+        />
+        <meta name="twitter:creator" content="@meta-check" />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/nomidman/image/upload/v1651493754/icon-192x192_xjiau9.webphttps://res.cloudinary.com/nomidman/image/upload/v1651510593/icon-192x192_zcm7e9.png"
+        />
+        <meta
+          name="og:image"
+          content="https://res.cloudinary.com/nomidman/image/upload/v1651493754/icon-192x192_xjiau9.webphttps://res.cloudinary.com/nomidman/image/upload/v1651510593/icon-192x192_zcm7e9.png"
+        />
+        <meta
+          name="og:image:secure_url"
+          content="https://res.cloudinary.com/nomidman/image/upload/v1651493754/icon-192x192_xjiau9.webphttps://res.cloudinary.com/nomidman/image/upload/v1651510593/icon-192x192_zcm7e9.png"
+        />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://res.cloudinary.com/nomidman/image/upload/v1651493754/icon-192x192_xjiau9.webphttps://res.cloudinary.com/nomidman/image/upload/v1651510593/icon-192x192_zcm7e9.png"
+        />
+        <meta property="og:site_name" content="Meta-Check" />
+        <meta
+          property="og:image"
+          content="BasaKhujo: A property searching platform in Bangladesh"
+        />
+        <meta
+          property="og:description"
+          content="BasaKhujo is an accommodation solution provider platform that makes it possible to rent a house & mess without paying any brokerage."
+        />
+      </Head>
+      {/* <Head>
         <title>Meta Check</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
@@ -14,7 +74,7 @@ export default function Home() {
         />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="BasaKhujo" key="ogsitename" />
-        <meta property="og:url" content="https://www.basakhujo.com/" />
+        <meta property="og:url" content="https://meta-check.netlify.app/" />
         <meta
           property="og:image"
           content="https://res.cloudinary.com/nomidman/image/upload/v1651493754/icon-192x192_xjiau9.webphttps://res.cloudinary.com/nomidman/image/upload/v1651510593/icon-192x192_zcm7e9.png"
@@ -36,7 +96,7 @@ export default function Home() {
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> */}
 
       <main className={styles.main}>
         <h1 className={styles.title}>
